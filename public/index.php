c6079e846fb0f6af198ece0b43c1e02e51d51f9f
edyonil <edyonil@gmail.com>
2016-07-31 23:15:49 -0300

    Funcionando o DocrineODM com mongodb

--- public/index.php
+++ public/index.php
@@ -1,14 +1,58 @@
-<?php
+<?php require_once __DIR__ .'/../vendor/autoload.php';
 
+use Saxulum\DoctrineMongoDbOdm\Provider\DoctrineMongoDbOdmProvider;
+use Saxulum\DoctrineMongoDb\Provider\DoctrineMongoDbProvider;
 
-require_once __DIR__ .'/../vendor/autoload.php';
+use Silex\Application;
 
 $app = new Silex\Application();
 
 $app['debug'] = true;
 
-$app->get('/', function() {
-    return "My Stuff";
+
+$app->register(new DoctrineMongoDbProvider, array(
+    "mongodb.options" => array(
+        "server" => "mongodb://localhost:27017",
+        "options" => array(
+            "username" => "homestead",
+            "password" => "secret",
+            "db" => "mystuff"
+        ),
+    ),
+));
+
+$app->register(new DoctrineMongoDbOdmProvider, array(
+    "mongodbodm.dm.options" => array(
+        "database" => "mystuff",
+        "mappings" => array(
+            // Using actual filesystem paths
+            array(
+                "type" => "annotation",
+                "namespace" => "MyStuff",
+                "path" => __DIR__."../src/MyStuff",
+            )
+        ),
+    ),
+));
+
+Doctrine\ODM\MongoDB\Mapping\Driver\AnnotationDriver::registerAnnotationClasses();
+
+$app->get('/', function() use ($app) {
+
+    $dm = $app['mongodbodm.dm'];
+
+    $post = new \MyStuff\Entities\Posts();
+    $post->setTitle('Meu segundo post');
+    $post->setDescription('Adicionei meu segundo post');
+
+    $dm->persist($post);
+
+    $dm->flush();
+
+
+    return "Ola mundo";
+
+
 });
 
 $app->run();
