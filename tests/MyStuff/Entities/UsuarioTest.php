cc4cd622df0fbc2e941b8577de6dc97292b46ce6
alexrsg <alexrsg@gmail.com>
2016-08-02 10:58:41 -0300

    Inclusão do phpunit.

---
+++ tests/MyStuff/Entities/UsuarioTest.php
@@ -0,0 +1,10 @@
+<?php
+declare(strict_types = 1);
+
+class UsuarioTest extends TestCase
+{
+    public function testObterUsuario()
+    {
+        //
+    }
+}
---
+++ tests/TestCase.php
@@ -0,0 +1,7 @@
+<?php
+declare(strict_types = 1);
+
+class TestCase extends PHPUnit_Framework_TestCase
+{
+    //
+}
