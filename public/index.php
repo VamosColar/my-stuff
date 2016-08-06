6a072dfadb5febf6114e5ba78283dde317793f5e
Allan Barbosa <allanbarbosa@MacBook-Air-de-Allan.local>
2016-08-06 09:17:14 -0300

    Baixando atualizações

--- public/index.php
+++ public/index.php
@@ -1,10 +1,11 @@
 <?php
 declare(strict_types = 1);
 
-require_once __DIR__ .'/../vendor/autoload.php';
+//require_once __DIR__ .'/../vendor/autoload.php';
+require_once __DIR__ .'/../app/config/app.php';
 
 
-$app = new \MyStuff\Application([]);
+//$app = new \MyStuff\Application([]);
 
 $app['debug'] = true;
 
