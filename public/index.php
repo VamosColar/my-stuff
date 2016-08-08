7639022e8ea201c9af2757b8d480465e77cb7a50
Allan Barbosa <allanbarbosa@air-de-allan.conder.intranet>
2016-08-08 09:16:20 -0300

    Modificado o nome da classe EntityInterface e alterado a forma de conexão ao banco

--- public/index.php
+++ public/index.php
@@ -1,8 +1,7 @@
 <?php
 declare(strict_types = 1);
 
-//require_once __DIR__ .'/../vendor/autoload.php';
-require_once __DIR__ .'/../app/config/app.php';
+require_once __DIR__ .'/../vendor/autoload.php';
 
 $app = new \MyStuff\Application([]);
 
